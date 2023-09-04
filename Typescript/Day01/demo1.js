e028e1fd013c176952c70c02fa929a85d69d2fba
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2023-09-04 13:04:21 +0530

    Day01

---
+++ Typescript/Day01/demo1.js
@@ -0,0 +1,26 @@
+// console.log("Hello World");
+// console.log('Hopes So U r Enjoying')
+// console.log(`
+//            U r Learning Anguylar 12
+// `);
+var a1, a2;
+var a_1, a$1;
+// Data Type
+// number(int,long,double,float,short),string(''/""),boolean(true/false)
+// any => (number,string,boolean)
+//void => it is neutral data => it can not point any thing
+// misce. null & undefined => It is used data(value) as well as type 
+var a = 10; // Forward declaration & defination 
+// console.log("Value of a "+a);
+// console.log(`Value of a ${a}`);
+var str;
+// console.log('Value of str is '+str);
+str = 'Sumit';
+// console.log('Value of str is '+str);
+var strAny;
+strAny = 'Sumit';
+console.log("Value of strAny " + strAny);
+strAny = 2.5;
+console.log("Value of strAny " + strAny);
+strAny = true;
+console.log("Value of strAny " + strAny);
---
+++ Typescript/Day01/demo1.ts
@@ -0,0 +1,51 @@
+
+// console.log("Hello World");
+// console.log('Hopes So U r Enjoying')
+// console.log(`
+
+//            U r Learning Anguylar 12
+// `);
+
+var a1,a2;
+var a_1,a$1;
+
+// Data Type
+// number(int,long,double,float,short),string(''/""),boolean(true/false)
+// any => (number,string,boolean)
+//void => it is neutral data => it can not point any thing
+// misce. null & undefined => It is used data(value) as well as type 
+
+var a:number=10; // Forward declaration & defination 
+// console.log("Value of a "+a);
+// console.log(`Value of a ${a}`);
+
+var str!:string;
+// console.log('Value of str is '+str);
+str='Sumit';
+// console.log('Value of str is '+str);
+
+var strAny:any;
+strAny='Sumit'
+// console.log("Value of strAny "+strAny);
+strAny=2.5;
+// console.log("Value of strAny "+strAny);
+strAny=true;
+// console.log("Value of strAny "+strAny);
+
+// Literal data type storing
+
+var b:number| string;
+b=3.5;
+b='Sumit';
+// b=true;
+
+var c:10 | boolean;
+c=10;
+c=true;
+var d:null | boolean | undefined;
+d=null;
+
+
+
+
+
